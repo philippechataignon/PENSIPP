5bf47496a78c6681cb5abd1804a5e066dec84088
Simon Rabaté <simonrabate@nat-oitwireless-inside-vapornet100-c-13161.princeton.edu>
2013-05-04 22:50:09 -0400

    Actualisation programmes JEM

--- Simulations/CN/CN_MACRO/DiscountDroits2.R
+++ Simulations/CN/CN_MACRO/DiscountDroits2.R
@@ -1,9 +1,10 @@
 ############################### TRANSITIONS VERS UN SYSTEME DE COMPTES NOTIONNELS ###################
-# Valorisation des cotisations passees. 
+# Equilibrage du régime à court terme (disparition de la bosse)
 # A partir de la date AnneeDebCN, tous les droits sont calcules dans le nouveau systeme. 
 # Hypothese de depart en retraite: meme age que dans le scenario de reference
-
-# Taux de cotisation: 27% sans ANC
+# Taux de cotisation 23 % + ANC
+# Gestion de la bosse de début de période. 
+# Réduction des pensions : 90% des droits acquis seulement. 
 
 t0  <- Sys.time()
 
@@ -28,11 +29,11 @@ graph_compar <- function (serie,t1,t2,titre)
 
 # Declaration des variable d'outputs
 TRC         <- numeric(taille_max)        # Taux de remplacemnt cible des liquidants
-ageliq_     <- matrix(nrow=taille_max,ncol=4)
+ageliq_     <- matrix(nrow=taille_max,ncol=3)
 duree_liq   <- numeric(taille_max)
-dar_        <- matrix(nrow=taille_max,ncol=4)
-pliq_    <- matrix(nrow=taille_max,ncol=4)
-pliq_rg     <- matrix(nrow=taille_max,ncol=4)
+dar_        <- matrix(nrow=taille_max,ncol=3)
+pliq_rg     <- matrix(nrow=taille_max,ncol=3)
+pliq_     <- matrix(nrow=taille_max,ncol=3)
 points_cn   <- numeric(taille_max)
 pension_cn  <- numeric(taille_max)
 conv        <- numeric(taille_max)
@@ -65,15 +66,12 @@ AGELIQ      <- matrix(nrow=4,ncol=160)    # Age de liquidation moyen par année
 
 
 W           <- 2047.501
-cibletaux<-numeric(taille_max)
-
-
 
 
 #### Début de la simulation ####
 
-#  Rprof(tmp<-tempfile())
-for (sc in c(1,2,3,4))
+
+for (sc in c(3))
   
 {
   
@@ -86,8 +84,9 @@ for (sc in c(1,2,3,4))
   points_cn   <- rep(0,taille_max)
   pension_cn  <- rep(0,taille_max)
   
-  plafond <- 8
-  UseOptCN(c("nomccn","nobonifcn","nomdacn","noavpfcn","noassimilcn"))
+  if (sc==3) {UseOptCN(c("discount"))}
+
+  plafond<-8
   
   for (t in 60:160)   # Début boucle temporelle
   {
@@ -95,18 +94,20 @@ for (sc in c(1,2,3,4))
     if (sc>1)
     {
       AnneeDepartCN <- 115
-      TauxCotCN[t]                 <- 0.258 #MPENS[1,115]/MSAL[1,115]
+      TauxCotCN[t]                 <- 0.23
       if (t <110) {RendementCN[t]  <- PIB[t]/PIB[t-1]-1} 
       else        {RendementCN[t]  <- log((MSAL[sc,t-1]*Prix[t-1])/(MSAL[sc,t-6]*Prix[t-6]))/5}
       RendementCNPrev[t]           <- RendementCN[t]
       RevaloCN[t+1]                <- Prix[t]/Prix[t-1]
       UseConv(55,70,t)
-      #  print (CoeffConv[60:80]) 
+      #  print (CoeffConv[60:80])   
     }
+    
+
     
     if (sc>1 && t==AnneeDepartCN)
     {
-      for (i in 1:55000)
+      for (i in 1:taille_max)
       {
         if (ageliq[i]==0)
         {
@@ -118,7 +119,7 @@ for (sc in c(1,2,3,4))
     
     
     # Liquidations  
-    for (i in 1:55000)       # Début boucle individuelle
+    for (i in 1:taille_max)       # Début boucle individuelle
     {
       
       # Liquidation
@@ -141,10 +142,10 @@ for (sc in c(1,2,3,4))
         
         if (t_liq[i]==t)
         {
-          pliq_[i,sc]   <- pension[i]
           points_cn[i]  <- points_cn_pri+points_cn_fp+points_cn_ind
           pension_cn[i] <- pension_cn_pri[i]+pension_cn_fp[i]+pension_cn_ind[i]
           pliq_rg[i,sc] <- pension_rg[i]
+          pliq_[i,sc] <- pension[i]
           dar_[i,sc]    <- dar[i]
           duree_liq[i]  <- duree_tot
           if (points_cn[i]>0) {conv[i] <- pension_cn[i]/points_cn[i]}
@@ -175,20 +176,6 @@ for (sc in c(1,2,3,4))
       PLIQ_IN[sc,t]   <- mean(pension_in[liquidants])
       PLIQ_CN[sc,t]   <- mean(pension_cn[liquidants])
       SALMOY[sc,t]    <- mean (salaire[actifs,t]/Prix[t])
-      ### Correction des salaires s'il y a lieu
-      ### NB : on redresse au fur et à mesure l'ensemble des salaires prospectifs, pour minimiser
-      ### redressement à chaque date
-      if (sc == 3 && t>=112) 
-      {
-        salaire[,t:160] <- salaire[,t:160]*SALMOY[sc,t-1]*1.01/SALMOY[sc,t]
-        PlafondSS[t+1]  <- PlafondSS[t]*1.01
-      }
-      if (sc == 4 && t>=112) 
-      {
-        salaire[,t:160] <- salaire[,t:160]*SALMOY[sc,t-1]*1.02/SALMOY[sc,t]
-        PlafondSS[t+1]  <- PlafondSS[t]*1.02
-      }    
-      SALMOY[sc,t]       <- mean (salaire[actifs,t]/Prix[t])    
       MPENS[sc,t]        <- W*sum(pension[retraites])/Prix[t]     
       MSAL[sc,t]         <- W*sum(salaire[actifs,t])/Prix[t] 
       PIBREF[sc,t]       <- MSAL[sc,t]*(PIB[109]/Prix[109])/MSAL[sc,109]
@@ -210,44 +197,16 @@ for (sc in c(1,2,3,4))
 } # Fin boucle scenarios
 
 
+save.image(paste0(cheminsource,"Simulations/CN/CNeq2(95).RData"))
+
 
 #### Sorties ####
-graph_compar(RATIOPENS       ,110,159,"Ratio pension/PIB")
-graph_compar(RATIOFIN        ,110,159,"Ratio Financier")
-
-save.image(paste0(cheminsource,"Simulations/CN/CN_MACRO/CN1.RData"))
-
-# Graphique3
 par(mar=c(6.1, 3.1, 4.1, 2.1))
 par(xpd=TRUE)
-plot   (seq(2010,2059,by=1),RATIOPENS[1,110:159],xlab="Annee", ylab="ratio retraite/PIB",ylim=c(0.11,0.13),col="grey0",lwd=4,type="l")
-points (seq(2010,2059,by=1),RATIOPENS[2,110:159],lwd=4,col="grey80",type="l")
-title("Graphe 3 : Evolution du ratio retraites/PIB \n Comparaison Système actuel - Comptes notionnels \n(27%, no ANC)", cex.main = 0.9)
-legend.text <- c("Régime actuel","Comptes notionnels")
-legend("bottom",inset=c(-0.2,-0.55),cex=0.8,legend.text, fill=c("grey0","grey80"))
-
-# Graphique3bis
-par(mar=c(6.1, 3.1, 4.1, 2.1))
-par(xpd=TRUE)
-graph_compar(RATIOPENS       ,110,159,"Ratio pension/PIB")
-title("Graphe 3 : Evolution du ratio retraites/PIB \n Comparaison Système actuel - Comptes notionnels \n(27%, no ANC)", cex.main = 0.9)
-legend.text <- c("Régime actuel","CN - scénario B","CN - scénario C","CN - scénario A")
-legend("bottom",inset=c(-0.2,-0.62),cex=0.8,legend.text, fill=c("orange","grey0","grey40","grey80"))
-
-RATIOPENSM   <- matrix(nrow=3,ncol=200)   
-for (t in 110:159){RATIOPENSM[,t]<-(RATIOPENS[2:4,(t-1)]+RATIOPENS[2:4,(t)]+RATIOPENS[2:4,(t+1)])/3}
-graph_compar(RATIOPENSM   ,110,159,"Ratio pension/PIB")
-title("Graphe 2 : Evolution du ratio retraite/PIB \nVariantes de scénario de croissance")
-legend.text<-c("Scenario B" ,"Scenario A" ,"Scenario C")
-legend("topleft",cex=0.9, legend.text,fill=c("grey0","grey40","grey80"))
-
-par(mar=c(6.1, 3.1, 4.1, 2.1))
-par(xpd=TRUE)
-plot   (seq(2010,2059,by=1),RATIOPENS[2,110:159],xlab="Annee", ylab="ratio retraite/PIB",ylim=c(0.11,0.15),col="grey0",lwd=4,type="l")
-points (seq(2010,2059,by=1),RATIOPENS[3,110:159],lwd=4,col="grey40",type="l")
-points (seq(2010,2059,by=1),RATIOPENS[4,110:159],lwd=4,col="grey80",type="l")
-title("Graphe 3 : Régime CN \nEvolution du ratio retraite/PIB \nVariantes de scénario de croissance", cex.main = 0.9)
-legend.text<-c("Scenario A (g=2%)","Scenario B (g=1.5%)","Scenario C (g=1%)")
-legend("bottom",inset=c(-0.2,-0.55),cex=0.8,legend.text, fill=c("grey80","grey0","grey40"))
-
+plot   (seq(2010,2059,by=1),RATIOFIN[1,110:159],xlab="Annee", ylab="ratio retraite/PIB",ylim=c(0.20,0.28),col="grey0",lwd=4,type="l")
+points (seq(2010,2059,by=1),RATIOFIN[2,110:159],lwd=4,col="grey80",type="l")
+points (seq(2010,2059,by=1),RATIOFIN[3,110:159],lwd=4,col="grey40",type="l")
+title("Graphe 6a : Evolution du ratio retraites/salaires \n(taux 23%, ANC)", cex.main = 0.9)
+legend.text <- c("Scénario de référénce","CN","CN réduction droits acquis")
+legend("bottom",inset=c(-0.2,-0.55),cex=0.8,legend.text, fill=c("grey0","grey80","grey40"))
 
